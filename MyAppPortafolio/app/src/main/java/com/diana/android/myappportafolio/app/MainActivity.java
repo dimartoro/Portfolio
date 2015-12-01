a0e6762f7ff8fef5b34a247ecbf32f7ff31e67ff
Diana Marlory Castro <dimartoro@gmail.com>
2015-11-30 21:47:50 -0500

    Task

---
+++ MyAppPortafolio/app/src/main/java/com/diana/android/myappportafolio/app/MainActivity.java
@@ -0,0 +1,94 @@
+package com.diana.android.myappportafolio.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void sendSpotifyStreamer(View view){
+        toast("This button will launch Spotify Streamer");
+    }
+
+
+    public void sendScoresApp(View view){
+        toast("This button will launch Scores App");
+    }
+
+
+    public void sendLibraryApp(View view){
+        toast("This button will launch Library App");
+    }
+
+
+    public void sendBuildItBigger(View view){
+        toast("This button will launch Build It Bigger");
+    }
+
+
+    public void sendXYZReader(View view){
+        toast("This button will launch XYZ Reader");
+    }
+
+
+    public void sendCapstoneMyOwnApp(View view){
+
+        toast("This button will launch my Capstone App");
+    }
+
+
+    private void toast(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
